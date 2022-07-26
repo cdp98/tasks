cc50c3f8523826b0cc27b6b8186272801f149920
cdp98 <pintodanielcarlos@gmail.com>
2022-07-26 16:04:36 -0300

    Creating models and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Task;
+use App\Models\Category;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks() {
+        return $this->hasMany(Tasks::class);
+    }
+
+    public function categories() {
+        return $this->hasMany(Tasks::class);
+    }
 }
